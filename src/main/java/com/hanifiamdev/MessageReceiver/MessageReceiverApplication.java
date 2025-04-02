b4818e22696f3b4d6cb8fd36d08e34698b502a6b
hanif.amrullah <hanif.amrullah@sigma.co.id>
2025-04-02 08:28:39 +0700

    Message Receiver ActiveMq Spring boot

--- src/main/java/com/hanifiamdev/MessageReceiver/MessageReceiverApplication.java
+++ src/main/java/com/hanifiamdev/MessageReceiver/MessageReceiverApplication.java
@@ -1,4 +1,4 @@
-package com.hanifiamdev.messagereceiver;
+package com.hanifiamdev.MessageReceiver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/hanifiamdev/MessageReceiver/service/MessageReceiver.java
@@ -0,0 +1,13 @@
+package com.hanifiamdev.MessageReceiver.service;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageReceiver {
+
+    @JmsListener(destination = "learning")
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+    }
+}
